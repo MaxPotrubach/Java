d7cb7b0a35a776f40849643c7aabb077bb27bb7b
DESKTOP-DN60QRR\ПК <st8645855@stud.duikt.edu.ua>
2025-12-18 02:23:57 +0200

    Added pract 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public  static void main(String[] args) {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
         try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter first number: ");
+
             System.out.print("Enter first number: ");
             double a = Double.parseDouble(input.nextLine());
 
-            System.out.print("Choose operation(+, -, *, /, ^, |, №): ");
-            String operation = input.nextLine();
+            System.out.print("Choose operation (+, -, *, /, ^, №, |): ");
+            String operation = input.nextLine().trim();
 
-            System.out.print("Enter second number(or 0): ");
-            double b = Double.parseDouble(input.nextLine());
+            double b = 0;
 
+            if (!operation.equals("№") && !operation.equals("|")) {
+                System.out.print("Enter second number: ");
+                b = Double.parseDouble(input.nextLine());
+            }
 
             double res;
+
             try {
                 switch (operation) {
                     case "+":
@@ -28,7 +32,7 @@ public class Main {
                     case "*":
                         res = calculator.mul(a, b);
                         break;
-                    case  "/":
+                    case "/":
                         res = calculator.div(a, b);
                         break;
                     case "^":
@@ -41,23 +45,18 @@ public class Main {
                         res = calculator.abs(a);
                         break;
                     default:
-                        System.out.println("Invalid input");
+                        System.out.println("Invalid operation selected");
                         return;
                 }
-
                 System.out.println("Result: " + res);
 
+            } catch (ArithmeticException e) {
+                System.out.println("Math Error: " + e.getMessage());
+            } catch (InvalidInputException e) {
+                System.out.println("Input Error: " + e.getMessage());
             }
-            catch (ArithmeticException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-            catch (InvalidInputException e) {
-                System.out.println("Invalid input: " + e.getMessage());
-            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input format (please enter a number)");
         }
-        catch (NumberFormatException e) {
-            System.out.println("Invalid input format");
-        }
-
     }
 }
